39bf679360c5d3d1d0b312231a0d841cebef34be
alexquar <alexanderquarrie@gmail.com>
2024-03-20 15:27:10 -0400

    logout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,13 +4,13 @@
       <h1>My Book List</h1>
 
       <!-- for logged in users -->
-      <div id="thing2">
+      <div>
         <router-link to="/">Home</router-link>
         <button @click="handleClick">Logout</button>
       </div>
       
       <!-- for logged out users -->
-      <div id="thing1">
+      <div>
         <router-link to="/login">Login</router-link>
         <router-link to="/signup">Signup</router-link>
       </div>
@@ -19,16 +19,18 @@
 </template>
 
 <script>
+import { auth } from '../firebase/config'
+import { signOut } from 'firebase/auth'
+
 export default {
-setup(){
+  setup() {
+    const handleClick = () => {
+      signOut(auth)
+      console.log('signed out')
+    }
 
-
-
-  const handleClick = () => {
-
+    return { handleClick }
   }
-  return {handleClick}
-}
 }
 </script>
 
