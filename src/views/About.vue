9c3233d930455c97fa4b79d2aa4b1ddd5fae8d0a
alexquar <alexanderquarrie@gmail.com>
2024-04-13 17:50:10 -0400

    about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
-
-    </div>
+        <h3>About the site...</h3>
+        <h5><a href="https://github.com/alexquar/U-books">Source code here!</a></h5>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut ex, nobis sequi natus nostrum molestiae nisi tempora dolore maiores ipsum cum eaque vitae ducimus voluptatem modi sunt in enim quam harum repellat alias, esse ab? Quae error dolorem, cum iusto cumque laudantium sapiente in nam ut laboriosam recusandae deserunt iste? Delectus non est nemo adipisci saepe esse consequuntur id porro temporibus molestias eius itaque odio dolore suscipit, vero necessitatibus explicabo velit inventore beatae deserunt dignissimos voluptate. Unde minus doloribus, debitis, quod magni tempora at aperiam illo dolorum porro veniam ab molestiae reprehenderit et ex. Aut ex laborum mollitia sequi nemo.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut ex, nobis sequi natus nostrum molestiae nisi tempora dolore maiores ipsum cum eaque vitae ducimus voluptatem modi sunt in enim quam harum repellat alias, esse ab? Quae error dolorem, cum iusto cumque laudantium sapiente in nam ut laboriosam recusandae deserunt iste? Delectus non est nemo adipisci saepe esse consequuntur id porro temporibus molestias eius itaque odio dolore suscipit, vero necessitatibus explicabo velit inventore beatae deserunt dignissimos voluptate. Unde minus doloribus, debitis, quod magni tempora at aperiam illo dolorum porro veniam ab molestiae reprehenderit et ex. Aut ex laborum mollitia sequi nemo.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut ex, nobis sequi natus nostrum molestiae nisi tempora dolore maiores ipsum cum eaque vitae ducimus voluptatem modi sunt in enim quam harum repellat alias, esse ab? Quae error dolorem, cum iusto cumque laudantium sapiente in nam ut laboriosam recusandae deserunt iste? Delectus non est nemo adipisci saepe esse consequuntur id porro temporibus molestias eius itaque odio dolore suscipit, vero necessitatibus explicabo velit inventore beatae deserunt dignissimos voluptate. Unde minus doloribus, debitis, quod magni tempora at aperiam illo dolorum porro veniam ab molestiae reprehenderit et ex. Aut ex laborum mollitia sequi nemo.</p>
+   </div>
 </template>
 
 <script>
@@ -15,5 +19,8 @@ export default {
 </script>
 
 <style scoped>
-
+a{
+    color: #a6a813; 
+    text-decoration: none;
+}
 </style>
